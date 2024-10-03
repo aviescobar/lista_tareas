718f63d60a0719e026748a73ef74353cb5a1e03c
aviescobar <91568563+aviescobar@users.noreply.github.com>
2024-10-02 22:07:09 -0600

    Update lista_tareas.py

--- lista_tareas.py
+++ lista_tareas.py
@@ -1,2 +1,13 @@
 class Tarea:
+   def __init__(self, descripcion):
+        self.descripcion = descripcion
+        self.completada = False
+
+    def completar(self):
+        self.completada = True
+
+    def __str__(self):
+        estado = "Completada" if self.completada else "Pendiente"
+        return f"{self.descripcion} - {estado}"
+      
   
